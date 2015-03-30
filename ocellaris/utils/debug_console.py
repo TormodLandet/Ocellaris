8bdf78d4b744c3770e407f8285b8d6f87751e2b2
Tormod Landet <tormod@landet.net>
2015-03-30 16:29:42 +0200

    Add easy to use debug plot command

--- ocellaris/utils/debug_console.py
+++ ocellaris/utils/debug_console.py
@@ -18,9 +18,17 @@ def debug_console_hook(simulation):
     while has_input(): 
         line = sys.stdin.readline()
         command = line.strip().lower()
+        
         if command == 'd':
             # d == "debug" -> start the debug console
             return run_debug_console(simulation)
+        
+        elif command == 'p':
+            # p == "plot" -> plot field variable
+            funcs, _ = define_convenience_functions(simulation)
+            simulation.log.info('\nCommand line action:\n  Plotting fields')
+            funcs['plot_all']()
+        
         elif command == 's':
             # s == "stop" -> stop the simulation
             simulation.log.info('\nCommand line action:\n  Setting simulation '
@@ -52,12 +60,38 @@ def run_debug_console(simulation):
         banner[-1] += '  %-18s' % name
     banner.append('\n\nPress Ctrl+D to continue running the simulation.'
                   '\nRunning exit() or quit() will stop Ocellaris.')
-    banner.append('\n>>> from dolfin import *')
+    
+    # Add some convenience functions
+    funcs, info = define_convenience_functions(simulation)
+    debug_locals.update(funcs)
+    banner.extend(info)
     
     # Setup tab completion
     readline.set_completer(rlcompleter.Completer(debug_locals).complete)
     readline.parse_and_bind("tab: complete")
     
     print '=== OCELLARIS CONSOLE === '*3
+    banner.append('\n>>> from dolfin import *')
     code.interact('\n'.join(banner), local=debug_locals)
     print '= OCELLARIS CONSOLE ===='*3
+
+def define_convenience_functions(simulation):
+    """
+    Some functions that are nice to have for debugging purposes
+    """
+    import dolfin
+    
+    info = []
+    funcs = {}
+    
+    # Convenience plotting function
+    fields = [name for name in ('u', 'p', 'c') if name in simulation.data]
+    info.append('Running plot_all() will plot %s' % ' & '.join(fields))
+    def plot_all():
+        for name in fields:
+            field = simulation.data[name]
+            dolfin.plot(field, title=name)
+        dolfin.interactive()
+    funcs['plot_all'] = plot_all
+    
+    return funcs, info
