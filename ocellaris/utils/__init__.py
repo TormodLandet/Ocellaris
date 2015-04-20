a2d12d2f8440f82255a5defcbe6677c030a4f084
Tormod Landet <tormod@landet.net>
2015-04-20 13:16:24 +0200

    Reorder imports to allow starting the debug console in user code

--- ocellaris/utils/__init__.py
+++ ocellaris/utils/__init__.py
@@ -1,8 +1,8 @@
 from .error_handling import report_error
+from .debug_console import debug_console_hook, run_debug_console
 from .timer import timeit
 from .code_runner import RunnablePythonString, CodedExpression
 from .cpp_expression import OcellarisCppExpression, ocellaris_project
 from .gradient_reconstruction import GradientReconstructor
 from .dofmap import facet_dofmap
-from .debug_console import debug_console_hook, run_debug_console
 from .linear_solvers import make_linear_solver, linear_solver_from_input
