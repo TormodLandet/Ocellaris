da7be927007c68630942d10a813aded69c6a1be7
Tormod Landet <tormod@landet.net>
2015-04-21 16:24:05 +0200

    Fix bug in the new DG0 facet expression. It used the wrong index in tha FacetFunction lookup

--- ocellaris/utils/dg_facet_expression.py
+++ ocellaris/utils/dg_facet_expression.py
@@ -5,10 +5,9 @@ def FacetExpressionDG0(mesh):
     Create a facet function that can be evaluated on facets in 
     ds and dS integrals
     """
-    facet_data = dolfin.FacetFunctionDouble(mesh)
     f = dolfin.Expression(code)
     f.mesh = mesh
-    f.facet_data = facet_data
+    f.facet_data = dolfin.FacetFunctionDouble(mesh)
     f.ocellaris_cpp_expression_type = 'FacetExpressionDG0'
     return f
 
@@ -33,22 +32,29 @@ public:
     void eval(Array<double>& values, const Array<double>& x,
               const ufc::cell& ufc_cell) const
     {
+        int ndim = (*mesh).geometry().dim();
         assert(facet_data);
-        assert(ufc_cell.geometric_dimension == (*mesh).geometry().dim());
+        assert(ufc_cell.geometric_dimension == ndim);
 
         if (ufc_cell.local_facet >= 0)
         {
+            // We are evaluated on a facet
+            // Get the facet index of the current facet 
             const Cell dolfin_cell(*mesh, ufc_cell.index);
-            int fidx = dolfin_cell.entities(1)[ufc_cell.local_facet];
+            int fidx = dolfin_cell.entities(ndim-1)[ufc_cell.local_facet];
             
             //cout << "at ufc_cell.index " << ufc_cell.index
             //     << " and local facet " << ufc_cell.local_facet
             //     << " and facet idx " << fidx << endl;
             
-            values[0] = (*facet_data)[ufc_cell.local_facet];
+            // Return the value provided in the facet_data mesh function
+            values[0] = (*facet_data)[fidx];
         }
         else
         {
+            // We are evaluated in a cell (dx integral), not on a facet
+            // (ds or dS integral). This should not happen. Report and
+            // return 0 
             not_on_boundary();
             values[0] = 0.0;
         }
@@ -61,8 +67,12 @@ private:
 '''
 
 if __name__ == '__main__':
-    mesh = dolfin.UnitSquareMesh(2, 2)
+    mesh = dolfin.UnitSquareMesh(3, 3, 'left/right')
     V = dolfin.FunctionSpace(mesh, 'CG', 1)
+    
+    x = mesh.coordinates()
+    x[:,0] = x[:,0]**2
+    x[:,1] = x[:,1]**3
 
     func = FacetExpressionDG0(mesh)
     func2 = dolfin.FacetArea(mesh)
@@ -78,4 +88,4 @@ if __name__ == '__main__':
     a2 = dolfin.assemble(func2('+')*v('+')*dolfin.dS).array()
     print a
     print a2
-    assert abs(a - a2 < 1e-10).all()
+    assert (abs(a - a2) < 1e-10).all()
