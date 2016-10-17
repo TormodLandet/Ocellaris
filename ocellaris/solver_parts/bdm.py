c89393b1a4183748fdef9dee17497711b30a2504
Tormod Landet <tormod@landet.net>
2016-10-17 13:04:16 +0200

    BDM: reimplement for DG1 and allow input function != output function

--- ocellaris/solver_parts/bdm.py
+++ ocellaris/solver_parts/bdm.py
@@ -1,12 +1,12 @@
 # encoding: utf8
 import dolfin
 from dolfin import FiniteElement, VectorElement, MixedElement, FunctionSpace, VectorFunctionSpace
-from dolfin import FacetNormal, TrialFunction, TestFunctions, Function 
+from dolfin import FacetNormal, TrialFunction, TestFunction, TestFunctions, Function 
 from dolfin import dot, as_vector, dx, dS, LocalSolver
 
 
 class VelocityBDMProjection():
-    def __init__(self, simulation, w, incompressibility_flux_type='central', D12=None):
+    def __init__(self, simulation, w, incompressibility_flux_type='central', D12=None, degree=None):
         """
         Implement equation 4a and 4b in "Two new techniques for generating exactly
         incompressible approximate velocities" by Bernardo Cocburn (2009)
@@ -22,13 +22,86 @@ class VelocityBDMProjection():
 
         The flux type can be 'central' or 'upwind'
         """
-        ue = w[0].function_space().ufl_element()
-        k = 2
+        self.simulation = simulation
+        
+        V = w[0].function_space()
+        ue = V.ufl_element()
+        if degree is None:
+            self.degree = ue.degree()
+            self.Vout = V
+        else:
+            self.degree = degree
+            self.Vout = FunctionSpace(V.mesh(), 'DG', degree)
+        
         assert ue.family() == 'Discontinuous Lagrange'
-        assert ue.degree() == k 
+        assert self.degree in (1, 2)
         assert w.ufl_shape == (2,)
         assert incompressibility_flux_type in ('central', 'upwind')
         
+        if self.degree == 1:
+            self._setup_dg1_projection(w, incompressibility_flux_type, D12)
+        else:
+            self._setup_dg2_projection(w, incompressibility_flux_type, D12)
+    
+    def _setup_dg1_projection(self, w, incompressibility_flux_type, D12):
+        """
+        Implement the projection where the result is BDM embeded in a DG1 function
+        """
+        sim = self.simulation
+        k = 1
+        mesh = w[0].function_space().mesh()
+        V = VectorFunctionSpace(mesh, 'DG', k)
+        W = FunctionSpace(mesh, 'DGT', k)
+        n = FacetNormal(mesh)
+        
+        v1 = TestFunction(W)
+        u = TrialFunction(V)
+        
+        # The same fluxes that are used in the incompressibility equation
+        if incompressibility_flux_type == 'central':    
+            u_hat_dS = dolfin.avg(w)
+        elif incompressibility_flux_type == 'upwind':
+            w_nU = (dot(w, n) + abs(dot(w, n)))/2.0
+            switch = dolfin.conditional(dolfin.gt(w_nU('+'), 0.0), 1.0, 0.0)
+            u_hat_dS = switch*w('+') + (1 - switch)*w('-')
+        
+        if D12 is not None:
+            u_hat_dS += dolfin.Constant([D12, D12])*dolfin.jump(w, n)
+        
+        # Equation 1 - flux through the sides
+        a = L = 0
+        for R in '+-':
+            a += dot(u(R), n(R))*v1(R)*dS
+            L += dot(u_hat_dS, n(R))*v1(R)*dS
+        for d in range(2):
+            dirichlet_bcs = sim.data['dirichlet_bcs']['u%d' % d]
+            neumann_bcs = sim.data['neumann_bcs'].get('u%d' % d, [])
+            outlet_bcs = sim.data['outlet_bcs']
+            for dbc in dirichlet_bcs:
+                u_bc = dbc.func()
+                a += u[d]*n[d]*v1*dbc.ds()
+                L += u_bc*n[d]*v1*dbc.ds()
+            for nbc in neumann_bcs + outlet_bcs:
+                a += u[d]*n[d]*v1*nbc.ds()
+                L += w[d]*n[d]*v1*nbc.ds()
+        
+        # Equation 2 - internal shape   :   empty for DG1
+        # Equation 3 - BDM Phi          :   empty for DG1
+        
+        # Pre-factorize matrices and store for usage in projection
+        self.local_solver = LocalSolver(a, L)
+        self.local_solver.factorize()
+        self.temp_function = Function(V)
+        self.w = w
+        self.assigner0 = dolfin.FunctionAssigner(self.Vout, V.sub(0))
+        self.assigner1 = dolfin.FunctionAssigner(self.Vout, V.sub(1))
+    
+    def _setup_dg2_projection(self, w, incompressibility_flux_type, D12):
+        """
+        Implement the projection where the result is BDM embeded in a DG2 function
+        """
+        sim = self.simulation
+        k = 2
         mesh = w[0].function_space().mesh()
         V = VectorFunctionSpace(mesh, 'DG', k)
         n = FacetNormal(mesh)
@@ -59,9 +132,9 @@ class VelocityBDMProjection():
             a += dot(u(R), n(R))*v1(R)*dS
             L += dot(u_hat_dS, n(R))*v1(R)*dS
         for d in range(2):
-            dirichlet_bcs = simulation.data['dirichlet_bcs']['u%d' % d]
-            neumann_bcs = simulation.data['neumann_bcs'].get('u%d' % d, [])
-            outlet_bcs = simulation.data['outlet_bcs']
+            dirichlet_bcs = sim.data['dirichlet_bcs']['u%d' % d]
+            neumann_bcs = sim.data['neumann_bcs'].get('u%d' % d, [])
+            outlet_bcs = sim.data['outlet_bcs']
             for dbc in dirichlet_bcs:
                 u_bc = dbc.func()
                 a += u[d]*n[d]*v1*dbc.ds()
@@ -84,10 +157,10 @@ class VelocityBDMProjection():
         self.local_solver.factorize()
         self.temp_function = Function(V)
         self.w = w
-        self.assigner0 = dolfin.FunctionAssigner(w[0].function_space(), V.sub(0))
-        self.assigner1 = dolfin.FunctionAssigner(w[1].function_space(), V.sub(1))
+        self.assigner0 = dolfin.FunctionAssigner(self.Vout, V.sub(0))
+        self.assigner1 = dolfin.FunctionAssigner(self.Vout, V.sub(1))
     
-    def run(self):
+    def run(self, w=None):
         """
         Perform the projection based on the current state of the Function w
         """
@@ -95,6 +168,9 @@ class VelocityBDMProjection():
         self.local_solver.solve_local_rhs(self.temp_function)
         
         # Assign to w
+        w = self.w if w is None else w
+        
         U0, U1 = self.temp_function.split()
-        self.assigner0.assign(self.w[0], U0)
-        self.assigner1.assign(self.w[1], U1)
+        
+        self.assigner0.assign(w[0], U0)
+        self.assigner1.assign(w[1], U1)
