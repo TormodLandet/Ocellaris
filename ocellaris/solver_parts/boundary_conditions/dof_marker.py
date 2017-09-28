5c89d6e100a1b50f7e52f12a42e07394a7db2a12
Tormod Landet <tormod@landet.net>
2017-09-28 10:57:40 +0200

    Py3 iteration fixes

--- ocellaris/solver_parts/boundary_conditions/dof_marker.py
+++ ocellaris/solver_parts/boundary_conditions/dof_marker.py
@@ -55,7 +55,7 @@ def get_dof_region_marks(simulation, V):
                 dof_region_marks.setdefault(fdof, []).append(mark)
 
     # Treat all dofs in the same location in the same way
-    for fdof, regions in dof_region_marks.items():
+    for fdof, regions in list(dof_region_marks.items()):
         for dof2 in same_loc_dofs[fdof]:
             if dof2 not in dof_region_marks:
                 dof_region_marks[dof2] = regions
