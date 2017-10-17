4cb1b5496f82ee4cb544eeba0378e54249ebee09
Tormod Landet <tormod@landet.net>
2017-10-17 15:21:18 +0200

    Dependency fixes for RTD [skip ci]

--- documentation/conf.py
+++ documentation/conf.py
@@ -34,7 +34,7 @@ if os.environ.get('READTHEDOCS') == 'True':
         def __getattr__(cls, name):
             return MagicMock()
     
-    MOCK_MODULES = ['dolfin', 'numpy', 'matplotlib', 'matplotlib.collections',
+    MOCK_MODULES = ['dolfin', 'ufl', 'numpy', 'matplotlib', 'matplotlib.collections',
                     'matplotlib.tri', 'petsc4py', 'h5py', 'yaml', 'solenoidal']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
