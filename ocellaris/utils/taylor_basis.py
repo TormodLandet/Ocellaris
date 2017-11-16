dba25f742bd9bdb04c92abbe24c701be59754fe0
Tormod Landet <tormod@landet.net>
2017-11-16 10:13:54 +0100

    Taylor code cleanup

--- ocellaris/utils/taylor_basis.py
+++ ocellaris/utils/taylor_basis.py
@@ -160,20 +160,20 @@ def DG1_to_taylor_matrix_2D(V):
         D = (y2 - y3)*(x1 - x3) + (x3 - x2)*(y1 - y3)
         
         # Value at xc (also the cell average value)
-        A[icell,0,0] = 1/3
-        A[icell,0,1] = 1/3
-        A[icell,0,2] = 1/3
+        A[icell, 0, 0] = 1/3
+        A[icell, 0, 1] = 1/3
+        A[icell, 0, 2] = 1/3
         
         # d/dx
-        A[icell,1,0] = (y2 - y3)/D
-        A[icell,1,1] = (-y1 + y3)/D
-        A[icell,1,2] = (y1 - y2)/D
+        A[icell, 1, 0] = (y2 - y3)/D
+        A[icell, 1, 1] = (-y1 + y3)/D
+        A[icell, 1, 2] = (y1 - y2)/D
         
         # d/dy
-        A[icell,2,0] = (-x2 + x3)/D
-        A[icell,2,1] = (x1 - x3)/D
-        A[icell,2,2] = (-x1 + x2)/D
-
+        A[icell, 2, 0] = (-x2 + x3)/D
+        A[icell, 2, 1] = (x1 - x3)/D
+        A[icell, 2, 2] = (-x1 + x2)/D
+    
     return A
 
 
@@ -272,51 +272,51 @@ def DG2_to_taylor_matrix_2D(V):
         ((x1, y1), (x2, y2), (x3, y3)) = x
         D = (y2 - y3)*(x1 - x3) + (x3 - x2)*(y1 - y3)
         
-        # MODIFIED to give the average value, not the center value!
-        A[icell,0,3] = 1/3
-        A[icell,0,4] = 1/3
-        A[icell,0,5] = 1/3
+        # Cell average value
+        A[icell, 0, 3] = 1/3
+        A[icell, 0, 4] = 1/3
+        A[icell, 0, 5] = 1/3
         
         # d/dx
-        A[icell,1,0] = (y2 - y3)/(3*D)
-        A[icell,1,1] = (-y1 + y3)/(3*D)
-        A[icell,1,2] = (y1 - y2)/(3*D)
-        A[icell,1,3] = 4*(-y2 + y3)/(3*D)
-        A[icell,1,4] = 4*(y1 - y3)/(3*D)
-        A[icell,1,5] = 4*(-y1 + y2)/(3*D)
+        A[icell, 1, 0] = (y2 - y3)/(3*D)
+        A[icell, 1, 1] = (-y1 + y3)/(3*D)
+        A[icell, 1, 2] = (y1 - y2)/(3*D)
+        A[icell, 1, 3] = 4*(-y2 + y3)/(3*D)
+        A[icell, 1, 4] = 4*(y1 - y3)/(3*D)
+        A[icell, 1, 5] = 4*(-y1 + y2)/(3*D)
         
         # d/dy
-        A[icell,2,0] = (-x2 + x3)/(3*D)
-        A[icell,2,1] = (x1 - x3)/(3*D)
-        A[icell,2,2] = (-x1 + x2)/(3*D)
-        A[icell,2,3] = 4*(x2 - x3)/(3*D)
-        A[icell,2,4] = 4*(-x1 + x3)/(3*D)
-        A[icell,2,5] = 4*(x1 - x2)/(3*D)
+        A[icell, 2, 0] = (-x2 + x3)/(3*D)
+        A[icell, 2, 1] = (x1 - x3)/(3*D)
+        A[icell, 2, 2] = (-x1 + x2)/(3*D)
+        A[icell, 2, 3] = 4*(x2 - x3)/(3*D)
+        A[icell, 2, 4] = 4*(-x1 + x3)/(3*D)
+        A[icell, 2, 5] = 4*(x1 - x2)/(3*D)
         
         # d/dx^2
-        A[icell,3,0] = 4*(y2 - y3)**2/D**2
-        A[icell,3,1] = 4*(y1 - y3)**2/D**2
-        A[icell,3,2] = 4*(y1 - y2)**2/D**2
-        A[icell,3,3] = -8*(y1 - y2)*(y1 - y3)/D**2
-        A[icell,3,4] = 8*(y1 - y2)*(y2 - y3)/D**2
-        A[icell,3,5] = -8*(y1 - y3)*(y2 - y3)/D**2
+        A[icell, 3, 0] = 4*(y2 - y3)**2/D**2
+        A[icell, 3, 1] = 4*(y1 - y3)**2/D**2
+        A[icell, 3, 2] = 4*(y1 - y2)**2/D**2
+        A[icell, 3, 3] = -8*(y1 - y2)*(y1 - y3)/D**2
+        A[icell, 3, 4] = 8*(y1 - y2)*(y2 - y3)/D**2
+        A[icell, 3, 5] = -8*(y1 - y3)*(y2 - y3)/D**2
         
         # d/dy^2
-        A[icell,4,0] = 4*(x2 - x3)**2/D**2
-        A[icell,4,1] = 4*(x1 - x3)**2/D**2
-        A[icell,4,2] = 4*(x1 - x2)**2/D**2
-        A[icell,4,3] = -8*(x1 - x2)*(x1 - x3)/D**2
-        A[icell,4,4] = 8*(x1 - x2)*(x2 - x3)/D**2
-        A[icell,4,5] = -8*(x1 - x3)*(x2 - x3)/D**2
+        A[icell, 4, 0] = 4*(x2 - x3)**2/D**2
+        A[icell, 4, 1] = 4*(x1 - x3)**2/D**2
+        A[icell, 4, 2] = 4*(x1 - x2)**2/D**2
+        A[icell, 4, 3] = -8*(x1 - x2)*(x1 - x3)/D**2
+        A[icell, 4, 4] = 8*(x1 - x2)*(x2 - x3)/D**2
+        A[icell, 4, 5] = -8*(x1 - x3)*(x2 - x3)/D**2
         
         # d/dx*dy
-        A[icell,5,0] = -4*(x2 - x3)*(y2 - y3)/D**2
-        A[icell,5,1] = -4*(x1 - x3)*(y1 - y3)/D**2
-        A[icell,5,2] = -4*(x1 - x2)*(y1 - y2)/D**2
-        A[icell,5,3] = 4*((x1 - x2)*(y1 - y3) + (x1 - x3)*(y1 - y2))/D**2
-        A[icell,5,4] = -(4*(x1 - x2)*(y2 - y3) + 4*(x2 - x3)*(y1 - y2))/D**2
-        A[icell,5,5] = 4*((x1 - x3)*(y2 - y3) + (x2 - x3)*(y1 - y3))/D**2
-
+        A[icell, 5, 0] = -4*(x2 - x3)*(y2 - y3)/D**2
+        A[icell, 5, 1] = -4*(x1 - x3)*(y1 - y3)/D**2
+        A[icell, 5, 2] = -4*(x1 - x2)*(y1 - y2)/D**2
+        A[icell, 5, 3] = 4*((x1 - x2)*(y1 - y3) + (x1 - x3)*(y1 - y2))/D**2
+        A[icell, 5, 4] = -(4*(x1 - x2)*(y2 - y3) + 4*(x2 - x3)*(y1 - y2))/D**2
+        A[icell, 5, 5] = 4*((x1 - x3)*(y2 - y3) + (x2 - x3)*(y1 - y3))/D**2
+    
     return A
 
 
@@ -345,9 +345,9 @@ def taylor_to_DG1_matrix_2D(V):
         
         for i in range(3):
             dx, dy = x[i,0] - xc[0], x[i,1] - xc[1]
-            A[icell,i,0] = 1
-            A[icell,i,1] = dx
-            A[icell,i,2] = dy
+            A[icell, i, 0] = 1
+            A[icell, i, 1] = dx
+            A[icell, i, 2] = dy
     
     return A
 
@@ -378,10 +378,10 @@ def taylor_to_DG1_matrix_3D(V):
         
         for i in range(4):
             dx, dy, dz = x[i,0] - xc[0], x[i,1] - xc[1], x[i,2] - xc[2]
-            A[icell,i,0] = 1
-            A[icell,i,1] = dx
-            A[icell,i,2] = dy
-            A[icell,i,3] = dz
+            A[icell, i, 0] = 1
+            A[icell, i, 1] = dx
+            A[icell, i, 2] = dy
+            A[icell, i, 3] = dz
     
     return A
 
@@ -421,12 +421,12 @@ def taylor_to_DG2_matrix_2D(V):
         
         for i in range(6):
             dx, dy = x[i,0] - xc[0], x[i,1] - xc[1]
-            A[icell,i,0] = 1
-            A[icell,i,1] = dx
-            A[icell,i,2] = dy
-            A[icell,i,3] = dx**2/2 - bar_xx
-            A[icell,i,4] = dy**2/2 - bar_yy
-            A[icell,i,5] = dx*dy  - bar_xy
+            A[icell, i, 0] = 1
+            A[icell, i, 1] = dx
+            A[icell, i, 2] = dy
+            A[icell, i, 3] = dx**2/2 - bar_xx
+            A[icell, i, 4] = dy**2/2 - bar_yy
+            A[icell, i, 5] = dx*dy  - bar_xy
     
     return A
 
@@ -440,9 +440,10 @@ def taylor_to_DG2_matrix_2D(V):
 #   only and is just kept here to help us remember that it exists and
 #   does not have to be reinvented every time
 
-def produce_code_with_sympy_DG1_2D():
+
+def produce_code_with_sympy_2D(degree):
     """
-    Use sympy to derive the coefficients needed to convert from DG1 polynomials
+    Use sympy to derive the coefficients needed to convert from DG polynomials
     on a triangle to a Taylor basis by expressing the derivatives at the centre
     point of the triangle
     (Only used for generating the code above, not used while running Ocellaris)
@@ -457,39 +458,99 @@ def produce_code_with_sympy_DG1_2D():
     L3f = 1 - L1f - L2f
     L1, L2, L3 = Symbol('L1')(x, y), Symbol('L2')(x, y), Symbol('L3')(x, y)
     
-    replacements = [(L1.diff(x),  L1f.diff(x)),
-                    (L2.diff(x),  L2f.diff(x)),
-                    (L3.diff(x),  L3f.diff(x)),
-                    (L1.diff(y),  L1f.diff(y)),
-                    (L2.diff(y),  L2f.diff(y)),
-                    (L3.diff(y),  L3f.diff(y)),
-                    (L1, 1/S(3)),
-                    (L2, 1/S(3)),
-                    (L3, 1/S(3))]
-    
-    # Barycentric quadratic shape functions on a triangle
-    phi = [L1, L2, L3]
+    # Construct replacements
+    Ls = [L1, L2, L3]
+    Lfs = [L1f, L2f, L3f]
+    replacements = []
+    for LX, LXf in zip(Ls, Lfs):
+        replacements.append((LX.diff(x, x), LXf.diff(x, x)))
+        replacements.append((LX.diff(y, y), LXf.diff(y, y)))
+        replacements.append((LX.diff(x, y), LXf.diff(x, y)))
+        replacements.append((LX.diff(x), LXf.diff(x)))
+        replacements.append((LX.diff(y), LXf.diff(y)))
+        replacements.append((LX, 1/S(3)))
+    replacements.append((x, (x1 + x2 + x3)/3))
+    replacements.append((y, (y1 + y2 + y3)/3))
     
     def print_code(name, func, index):
         code = ['# %s' % name]
-        for i in range(3):
-            v = func(phi[i])
+        for i, phii in enumerate(phi):
+            v = func(phii)
             v = v.subs(replacements)
             v = v.simplify()
-            code.append('A[icell,%d,%d] = %s' % (index, i, v))
+            code.append('A[icell, %d, %d] = %s' % (index, i, v))
         code.append('')
         print('\n'.join(code))
     
-    print_code('Value at xc (also the cell average value)', lambda q: q, 0)
-    print_code('d/dx', lambda q: q.diff(x), 1)
-    print_code('d/dy', lambda q: q.diff(y), 2)
+    if degree == 1:
+        # Barycentric quadratic shape functions on a triangle
+        phi = [L1, L2, L3]
+        
+        print_code('Value at xc (also the cell average value)', lambda q: q, 0)
+        print_code('d/dx', lambda q: q.diff(x), 1)
+        print_code('d/dy', lambda q: q.diff(y), 2)
+    
+    elif degree == 2:
+        # Barycentric quadratic shape functions on a triangle
+        phi = [None]*6
+        phi[0] = 2*L1*(L1 - 1/S(2))
+        phi[1] = 2*L2*(L2 - 1/S(2))
+        phi[2] = 2*L3*(L3 - 1/S(2))
+        phi[3] = 4*L2*L3
+        phi[4] = 4*L3*L1
+        phi[5] = 4*L1*L2
+        
+        #print_code('Value at xc (MODIFY ME TO GET THE CELL AVERAGE)', lambda q: q, 0)
+        print('# Cell average value')
+        print('A[icell, 0, 3] = 1/3')
+        print('A[icell, 0, 4] = 1/3')
+        print('A[icell, 0, 5] = 1/3\n')
+        print_code('d/dx', lambda q: q.diff(x), 1)
+        print_code('d/dy', lambda q: q.diff(y), 2)
+        print_code('d/dx^2', lambda q: q.diff(x, x), 3)
+        print_code('d/dy^2', lambda q: q.diff(y, y), 4)
+        print_code('d/dx*dy', lambda q: q.diff(x, y), 5)
+        
+        # Cell average terms
+        BT_QUADRATURE_TABLE = {}
+        BT_QUADRATURE_TABLE[1] = ([[1/S(3), 1/S(3)]],
+                                  [1.0])
+        BT_QUADRATURE_TABLE[2] = ([[2/S(3), 1/S(6)], [1/S(6), 2/S(3)], [1/S(6), 1/S(6)]],
+                                  [1/S(3), 1/S(3), 1/S(3)])
+        BT_QUADRATURE_TABLE[3] = ([[1/S(3), 1/S(3)], [S(3)/5, S(1)/5], [S(1)/5, S(3)/5], [S(1)/5, S(1)/5]],
+                                  [-S(9)/16, S(25)/48, S(25)/48, S(25)/48])
+        
+        xv = x1*L1 + x2*L2 + x3*L3
+        yv = y1*L1 + y2*L2 + y3*L3
+        xc = xv.subs({L1: 1/S(3), L2: 1/S(3), L3: 1/S(3)})
+        yc = yv.subs({L1: 1/S(3), L2: 1/S(3), L3: 1/S(3)})
+        
+        exprs = [('bar_xx', (xv-xc)**2/2),
+                 ('bar_yy', (yv-yc)**2/2),
+                 ('bar_xy', (xv-xc)*(yv-yc))]
+        for i in range(6):
+            exprs.append(('A[icell,0,%d]' % i, phi[i]))
+        
+        order = 3
+        points, weights = BT_QUADRATURE_TABLE[order]
+        for name, expr in exprs:
+            v = 0
+            for point, weight in zip(points, weights):
+                loc = {L1: point[0],
+                       L2: point[1],
+                       L3: 1 - point[0] - point[1]}
+                v += weight*expr.subs(loc)
+            v = v.simplify().simplify()
+            v = v.subs({x1: Symbol('x1'), x2: Symbol('x2'), x3: Symbol('x3'),
+                        y1: Symbol('y1'), y2: Symbol('y2'), y3: Symbol('y3')})
+            print('%s = %s' % (name, v))
 
 
-def produce_code_with_sympy_DG1_3D(degree=1):
+def produce_code_with_sympy_3D(degree):
     """
-    Use sympy to derive the coefficients needed to convert from DG1 polynomials
-    on a tetrahedron to a Taylor basis by expressing the derivatives at the centre
-    point of the triangle
+    Use sympy to derive the coefficients needed to convert from DG polynomials
+    on a tetrahedron to a Taylor basis by expressing the derivatives at the
+    centre point in the tetrahedron
     (Only used for generating the code above, not used while running Ocellaris)
     """
     from sympy import Symbol, symbols, S
@@ -525,155 +586,58 @@ def produce_code_with_sympy_DG1_3D(degree=1):
     #L4f = 1 - L1f - L2f - L3f 
     L1, L2, L3, L4 = Symbol('L1')(x, y, z), Symbol('L2')(x, y, z), Symbol('L3')(x, y, z), Symbol('L4')(x, y, z)
     
-    replacements = [(L1.diff(x),  L1f.diff(x)),
-                    (L2.diff(x),  L2f.diff(x)),
-                    (L3.diff(x),  L3f.diff(x)),
-                    (L4.diff(x),  L4f.diff(x)),
-                    (L1.diff(y),  L1f.diff(y)),
-                    (L2.diff(y),  L2f.diff(y)),
-                    (L3.diff(y),  L3f.diff(y)),
-                    (L4.diff(y),  L4f.diff(y)),
-                    (L1.diff(z),  L1f.diff(z)),
-                    (L2.diff(z),  L2f.diff(z)),
-                    (L3.diff(z),  L3f.diff(z)),
-                    (L4.diff(z),  L4f.diff(z)),
-                    (L1, 1/S(4)),
-                    (L2, 1/S(4)),
-                    (L3, 1/S(4)),
-                    (L4, 1/S(4)),
-                    (x, (x1 + x2 + x3 + x4)/4),
-                    (y, (y1 + y2 + y3 + y4)/4),
-                    (z, (z1 + z2 + z3 + z4)/4)]
+    # Construct replacements
+    Ls = [L1, L2, L3, L4]
+    Lfs = [L1f, L2f, L3f, L4f]
+    replacements = []
+    for LX, LXf in zip(Ls, Lfs):
+        replacements.append((LX.diff(x, x), LXf.diff(x, x)))
+        replacements.append((LX.diff(y, y), LXf.diff(y, y)))
+        replacements.append((LX.diff(z, z), LXf.diff(z, z)))
+        replacements.append((LX.diff(x, y), LXf.diff(x, y)))
+        replacements.append((LX.diff(x, z), LXf.diff(x, z)))
+        replacements.append((LX.diff(y, z), LXf.diff(y, z)))
+        replacements.append((LX.diff(x), LXf.diff(x)))
+        replacements.append((LX.diff(y), LXf.diff(y)))
+        replacements.append((LX.diff(z), LXf.diff(z)))
+        replacements.append((LX, 1/S(4)))
+    replacements.append((x, (x1 + x2 + x3 + x4)/4))
+    replacements.append((y, (y1 + y2 + y3 + y4)/4))
+    replacements.append((z, (z1 + z2 + z3 + z4)/4))
+    
+    def print_code(name, func, index):
+        code = ['# %s' % name]
+        for i, phii in enumerate(phi):
+            v = func(phii)
+            v = v.subs(replacements)
+            v = v.simplify()
+            vstr = str(v).replace('Abs', 'abs')
+            code.append('A[icell, %d, %d] = %s' % (index, i, vstr))
+        code.append('')
+        print('\n'.join(code))
     
     if degree == 1:
         # Barycentric linear shape functions on a triangle
         phi = [L1, L2, L3, L4]
         
-        print('V = %s' % Vf)
-        
-        def print_code(name, func, index):
-            code = ['# %s' % name]
-            for i in range(4):
-                v = func(phi[i])
-                v = v.subs(replacements)
-                v = v.simplify()
-                code.append('A[icell,%d,%d] = %s' % (index, i, v))
-            code.append('')
-            print('\n'.join(code))
-        
+        print('V = %s' % str(Vf).replace('Abs', 'abs'))
         print_code('Value at xc (also the cell average value)', lambda q: q, 0)
         print_code('d/dx', lambda q: q.diff(x), 1)
         print_code('d/dy', lambda q: q.diff(y), 2)
         print_code('d/dz', lambda q: q.diff(z), 3)
     
     elif degree == 2:
-        
-
-
-def produce_code_with_sympy_DG2_2D():
-    """
-    Use sympy to derive the coefficients needed to convert from DG2 polynomials
-    on a triangle to a Taylor basis by expressing the derivatives at the centre
-    point of the triangle
-    (Only used for generating the code above, not used while running Ocellaris)
-    """
-    from sympy import Symbol, symbols, S
-    x1, x2, x3, y1, y2, y3 = symbols('x1 x2 x3 y1 y2 y3')
-    x, y, D = symbols('x y D')
-    
-    # Barycentric coordinates from cartesian corner coordinates
-    L1f = ((y2 - y3)*(x - x3) + (x3 - x2)*(y - y3))/D
-    L2f = ((y3 - y1)*(x - x3) + (x1 - x3)*(y - y3))/D
-    L3f = 1 - L1f - L2f
-    L1, L2, L3 = Symbol('L1')(x, y), Symbol('L2')(x, y), Symbol('L3')(x, y)
-    
-    replacements = [(L1.diff(x, x),  0),
-                    (L2.diff(x, x),  0),
-                    (L3.diff(x, x),  0),
-                    (L1.diff(y, y),  0),
-                    (L2.diff(y, y),  0),
-                    (L3.diff(y, y),  0),
-                    (L1.diff(x, y),  0),
-                    (L2.diff(x, y),  0),
-                    (L3.diff(x, y),  0),
-                    (L1.diff(x),  L1f.diff(x)),
-                    (L2.diff(x),  L2f.diff(x)),
-                    (L3.diff(x),  L3f.diff(x)),
-                    (L1.diff(y),  L1f.diff(y)),
-                    (L2.diff(y),  L2f.diff(y)),
-                    (L3.diff(y),  L3f.diff(y)),
-                    (L1, 1/S(3)),
-                    (L2, 1/S(3)),
-                    (L3, 1/S(3))]
-    
-    # Barycentric quadratic shape functions on a triangle
-    phi = [None]*6
-    phi[0] = 2*L1*(L1 - 1/S(2))
-    phi[1] = 2*L2*(L2 - 1/S(2))
-    phi[2] = 2*L3*(L3 - 1/S(2))
-    phi[3] = 4*L2*L3
-    phi[4] = 4*L3*L1
-    phi[5] = 4*L1*L2
-    
-    def print_code(name, func, index):
-        code = ['# %s' % name]
-        for i in range(6):
-            v = func(phi[i])
-            v = v.subs(replacements)
-            v = v.simplify()
-            code.append('A[icell,%d,%d] = %s' % (index, i, v))
-        code.append('')
-        print('\n'.join(code))
-    
-    print_code('Value at xc (MODIFY ME TO GET THE CELL AVERAGE)', lambda q: q, 0)
-    print_code('d/dx', lambda q: q.diff(x), 1)
-    print_code('d/dy', lambda q: q.diff(y), 2)
-    print_code('d/dx^2', lambda q: q.diff(x, x), 3)
-    print_code('d/dy^2', lambda q: q.diff(y, y), 4)
-    print_code('d/dx*dy', lambda q: q.diff(x, y), 5)
-    
-    # Cell average terms
-    BT_QUADRATURE_TABLE = {}
-    BT_QUADRATURE_TABLE[1] = ([[1/S(3), 1/S(3)]],
-                              [1.0])
-    BT_QUADRATURE_TABLE[2] = ([[2/S(3), 1/S(6)], [1/S(6), 2/S(3)], [1/S(6), 1/S(6)]],
-                              [1/S(3), 1/S(3), 1/S(3)])
-    BT_QUADRATURE_TABLE[3] = ([[1/S(3), 1/S(3)], [S(3)/5, S(1)/5], [S(1)/5, S(3)/5], [S(1)/5, S(1)/5]],
-                              [-S(9)/16, S(25)/48, S(25)/48, S(25)/48])
-    
-    xv = x1*L1 + x2*L2 + x3*L3
-    yv = y1*L1 + y2*L2 + y3*L3
-    xc = xv.subs({L1: 1/S(3), L2: 1/S(3), L3: 1/S(3)})
-    yc = yv.subs({L1: 1/S(3), L2: 1/S(3), L3: 1/S(3)})
-    
-    exprs = [('bar_xx', (xv-xc)**2/2),
-             ('bar_yy', (yv-yc)**2/2),
-             ('bar_xy', (xv-xc)*(yv-yc))]
-    for i in range(6):
-        exprs.append(('A[icell,0,%d]' % i, phi[i]))
-    
-    order = 3
-    points, weights = BT_QUADRATURE_TABLE[order]
-    for name, expr in exprs:
-        v = 0
-        for point, weight in zip(points, weights):
-            loc = {L1: point[0],
-                   L2: point[1],
-                   L3: 1 - point[0] - point[1]}
-            v += weight*expr.subs(loc)
-        v = v.simplify().simplify()
-        v = v.subs({x1: Symbol('x1'), x2: Symbol('x2'), x3: Symbol('x3'),
-                    y1: Symbol('y1'), y2: Symbol('y2'), y3: Symbol('y3')})
-        print('%s = %s' % (name, v))
+        pass
 
 
 if __name__ == '__main__':
-    funcs = (#produce_code_with_sympy_DG1_2D,
-             #produce_code_with_sympy_DG2_2D,
-             #produce_code_with_sympy_DG1_3D,
-             produce_code_with_sympy_DG2_3D,)
-    for func in funcs:
+    funcs = ((produce_code_with_sympy_2D, 1),
+             (produce_code_with_sympy_2D, 2),
+             (produce_code_with_sympy_3D, 1),
+             (produce_code_with_sympy_3D, 2),)
+    
+    for func, degree in funcs:
         print('='*80)
-        print(func.__name__ + '\n' + '-'*40 + '\n')
-        func()
+        print(func.__name__, 'degree', degree, '\n' + '-'*40 + '\n')
+        func(degree)
         print()
