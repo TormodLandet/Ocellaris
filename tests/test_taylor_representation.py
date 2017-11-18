660961e3990d24647279937d46ee8af379ebfa18
Tormod Landet <tormod@landet.net>
2017-11-18 17:36:20 +0100

    3D Taylor transform now seems to work
    Should look closer at that 6 factor in the tetrahedon quadrature one
    day... The quadrature rule should multiply by the volume, but we want
    the average so we are just going to divide by the same volume
    afterwards. There should hence not be any need for a factor?

--- tests/test_taylor_representation.py
+++ tests/test_taylor_representation.py
@@ -40,7 +40,7 @@ def test_taylor_projections_3D(degree):
     # Setup Lagrange function with random data
     f1 = dolfin.Function(V)
     N = f1.vector().local_size()
-    f1.vector().set_local(numpy.random.rand(N)*0+1)
+    f1.vector().set_local(numpy.random.rand(N))
     
     # Convert to Taylor representation
     f2 = dolfin.Function(V)
@@ -50,29 +50,8 @@ def test_taylor_projections_3D(degree):
     f3 = dolfin.Function(V)
     taylor_to_lagrange(f2, f3)
     
-    from ocellaris.utils.taylor_basis import CACHE
-    A1 = CACHE[('lagrange_to_taylor_matrices', degree)]
-    A2 = CACHE[('taylor_to_lagrange_matrices', degree)]
-    
-    A1i, A2i = A1[1], A2[1]
-    print('I:')
-    print(numpy.array_repr(numpy.dot(A1i, A2i), max_line_width=120, precision=4, suppress_small=True))
-    print(set(f3.vector().get_local()))
-    print('A1i')
-    print(numpy.array_repr(A1i, max_line_width=120, precision=4, suppress_small=True))
-    print('A2i.I')
-    print(numpy.array_repr(numpy.linalg.inv(A2i), max_line_width=120, precision=4, suppress_small=True))
-    print('A2i')
-    print(numpy.array_repr(A2i, max_line_width=120, precision=4, suppress_small=True))
-    print('A1i.I')
-    print(numpy.array_repr(numpy.linalg.inv(A1i), max_line_width=120, precision=4, suppress_small=True))
-    
     error = dolfin.errornorm(f1, f3, degree_rise=0)
-    print(error)
-    if degree == 1:
-        assert error < 1e-15
-    else:
-        return pytest.skip('Quadratic tetrahedra not quite working yet')
+    assert error < 1e-15
 
 
 def make_taylor_func(u, coeffs):
@@ -157,7 +136,7 @@ def make_taylor_func(u, coeffs):
 
 @pytest.mark.parametrize("dim", [2, 3])
 @pytest.mark.parametrize("degree", [1, 2])
-def test_taylor_values_2D(dim, degree):
+def test_taylor_values(dim, degree):
     """
     Check that the Lagrange -> Taylor projection gives the correct Taylor values
     """
@@ -182,9 +161,7 @@ def test_taylor_values_2D(dim, degree):
     # Check that the target values are obtained
     dm = V.dofmap()
     vals = t.vector().get_local()
-    print(coeffs, '<-- TARGET')
     for cell in dolfin.cells(mesh):
         cell_dofs = dm.cell_dofs(cell.index())
         cell_vals = vals[cell_dofs]
-        print(cell_vals)
         assert all(abs(cell_vals - coeffs) < 1e-13)
