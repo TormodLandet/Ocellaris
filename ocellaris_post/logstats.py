b6ecb6835f869c7998a4f184b4cb17f5d66e6c1a
Tormod Landet <tormod@landet.net>
2017-12-08 23:04:46 +0100

    ocellaris_post.logstats cosmetics

--- ocellaris_post/logstats.py
+++ ocellaris_post/logstats.py
@@ -12,12 +12,8 @@ def get_stats(name, ts):
     return (name, avg, std, ts.min(), ts.max())
 
 
-def show_logstats(filename):
-    print('Result file statistics')
-    print('File:', filename)
-    print()
-    
-    results = Results(filename)
+def show_logstats(file_name):
+    results = Results(file_name)
     maxlen = 0
     stats = []
     for rname, ts in results.reports.items():
@@ -31,21 +27,26 @@ def show_logstats(filename):
     template2 = template + ' |% 11.3g |% 9.2f %% |% 11.3g |% 11.3g |'
     sep = '+' + '-' * (maxlen + 54) + '+'
     
+    print(('%%%ds' % len(sep)) % file_name)
     print(sep)
     print(template % 'Name', '|       Mean |    Std.dev |        Min |        Max |')
     print(sep.replace('-', '='))
     i = 0
+    L = 3
     for s in sorted(stats):
-        if i % 5 == 0 and i != 0:
+        if i % L == 0 and i != 0:
             print(sep)
         print(template2 % s)
         i += 1
-    if i % 5 != 0:
+    if i % L != 0:
         print(sep)
     print()
 
 
 def main(args):
+    print('Result file statistics')
+    print('======================\n')
+    
     # Get report files to read
     file_names = []
     for fn in args[1:]:
