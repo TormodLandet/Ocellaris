1f916a9833764eeb81ea18d3eb2ececad25507ff
Tormod Landet <tormod@landet.net>
2018-07-13 18:24:30 +0200

    Preprocess data
    Split out work that only needs to be performed once per geometry

--- ocellaris/probes/free_surface_locator/vof_dg0_surface_locator.py
+++ ocellaris/probes/free_surface_locator/vof_dg0_surface_locator.py
@@ -8,14 +8,64 @@ class FreeSurfaceLocatorImplDG0:
         self.simulation = simulation
         self.field = c
         self.value = value
+        self.cache = preprocess(simulation, c)
 
     def compute_crossing_points(self):
-        return crossing_points_and_cells(self.simulation, self.field, self.value)
+        return crossing_points_and_cells(self.simulation, self.field, self.value, self.cache)
 
 
-def crossing_points_and_cells(simulation, field, value):
+def preprocess(simulation, field):
     """
-    Find cells that contain the value iso surface. This is done by 
+    Store DOF and geometry info that will not change unless the mesh is
+    updated (which is not handled in any way)
+    """
+    mesh = simulation.data['mesh']
+    dofmap = field.function_space().dofmap()
+    conFC = simulation.data['connectivity_FC']
+
+    # Data to compute
+    facet_data = {}
+    cell_dofs = [None] * mesh.num_cells()
+    is_ghost_cell = [False] * mesh.num_cells()
+
+    cell_coords = numpy.zeros((2, 3), float)
+    for facet in dolfin.facets(mesh, 'all'):
+        fid = facet.index()
+        cell_ids = conFC(fid)
+        if len(cell_ids) != 2:
+            continue
+
+        # Get midpoint coordinates of the two connected cells and the DG0
+        # dof needed to find the field value in each of the cells
+        for i, cell_id in enumerate(cell_ids):
+            cell = dolfin.Cell(mesh, cell_id)
+            is_ghost_cell[cell_id] = cell.is_ghost()
+
+            pt = cell.midpoint()
+            cell_coords[i, 0] = pt.x()
+            cell_coords[i, 1] = pt.y()
+            cell_coords[i, 2] = pt.z()
+
+            # Get the one and only DG0 dof for this cell
+            dofs = dofmap.cell_dofs(cell_id)
+            assert len(dofs) == 1
+            cell_dofs[cell_id] = dofs[0]
+
+        # Unit vector from cell 1 to cell 0
+        uvec = cell_coords[0] - cell_coords[1]
+        uvec /= (uvec[0] ** 2 + uvec[1] ** 2 + uvec[2] ** 2) ** 0.5
+
+        # Store data for later use
+        coords0 = numpy.array(cell_coords[0])
+        coords1 = numpy.array(cell_coords[1])
+        facet_data[fid] = cell_ids[0], cell_ids[1], coords0, coords1, uvec
+
+    return facet_data, cell_dofs, is_ghost_cell
+
+
+def crossing_points_and_cells(simulation, field, value, preprocessed):
+    """
+    Find cells that contain the value iso surface. This is done by
     connecting cell midpoints across facets and seing if the level set
     crosses this line. If it does, the point where it crosses, the cell
     containing the free surface crossing point and the vector from the low
@@ -31,61 +81,37 @@ def crossing_points_and_cells(simulation, field, value):
 
     The field is assumed to be piecewice constant (DG0)
     """
-    mesh = simulation.data['mesh']
+    facet_data, cell_dofs, is_ghost_cell = preprocessed
     all_values = get_local(field)
-    dofmap = field.function_space().dofmap()
-
-    # Mesh connectivities
-    conFC = simulation.data['connectivity_FC']
 
     # We define acronym LCCM: line connecting cell midpoints
     #   - We restrinct ourselves to LCCMs that cross only ONE facet
     #   - We number LLCMs by the index of the crossed facet
 
     # Find the crossing points where the contour crosses a LCCM
-    cell_coords = numpy.zeros((2, 3), float)
-    cell_values = numpy.zeros(2, float)
     crossing_points = {}
-    for facet in dolfin.facets(mesh):
-        fid = facet.index()
-        cell_ids = conFC(fid)
-        if len(cell_ids) != 2:
-            continue
+    for fid, fdata in facet_data.items():
+        # Get preprocessed data
+        cid0, cid1, coords0, coords1, uvec = fdata
 
-        is_ghost_cell = [False, False]
-        for i, cell_id in enumerate(cell_ids):
-            cell = dolfin.Cell(mesh, cell_id)
-            is_ghost_cell[i] = cell.is_ghost()
-
-            # LCCM endpoint coordinates
-            pt = cell.midpoint()
-            cell_coords[i, 0] = pt.x()
-            cell_coords[i, 1] = pt.y()
-            cell_coords[i, 2] = pt.z()
-
-            # LCCM endpoint values
-            dofs = dofmap.cell_dofs(cell_id)
-            assert len(dofs) == 1
-            cell_values[i] = all_values[dofs[0]]
-
-        b1, b2 = cell_values[0] < value, cell_values[1] < value
+        # Check for level crossing
+        v0 = all_values[cell_dofs[cid0]]
+        v1 = all_values[cell_dofs[cid1]]
+        b1, b2 = v0 < value, v1 < value
         if (b1 and b2) or not (b1 or b2):
             # LCCM not crossed by contour
             continue
 
         # Find the location where the contour line crosses the LCCM
-        v1, v2 = cell_values
-        fac = (v1 - value) / (v1 - v2)
-        crossing_point = tuple((1 - fac) * cell_coords[0] + fac * cell_coords[1])
+        fac = (v0 - value) / (v0 - v1)
+        crossing_point = tuple((1 - fac) * coords0 + fac * coords1)
 
         # Scaled direction vector
-        direction = cell_coords[0] - cell_coords[1]
-        direction /= (direction[0] ** 2 + direction[1] ** 2 + direction[2] ** 2) ** 0.5
-        direction *= v1 - v2
+        direction = uvec * (v0 - v1)
 
         # Find the cell containing the contour line
-        surf_cid = cell_ids[0] if fac <= 0.5 else cell_ids[1]
-        is_ghost = is_ghost_cell[0] if fac <= 0.5 else is_ghost_cell[1]
+        surf_cid = cid0 if fac <= 0.5 else cid1
+        is_ghost = is_ghost_cell[cid0] if fac <= 0.5 else is_ghost_cell[cid1]
 
         if not is_ghost:
             # Store the point and direction towards the high value cell
