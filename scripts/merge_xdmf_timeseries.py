135a00bc4f80f986b3ae0ead3cda154e9ceb0f8b
Tormod Landet <tormod@landet.net>
2018-08-21 16:27:05 +0200

    Use xml.etree and h5py - HDF5 files are now also merged

--- scripts/merge_xdmf_timeseries.py
+++ scripts/merge_xdmf_timeseries.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import h5py
+from xml.etree import ElementTree as ET
 
 
 def merge_xdmf_timeseries(inp_file_names, out_file_name, verbose=True):
@@ -13,67 +15,92 @@ def merge_xdmf_timeseries(inp_file_names, out_file_name, verbose=True):
     The current code is quite hacky and probably only works for input XDMF
     files written by the current (2018.1) version of DOLFIN
     """
-    timesteps = []
+    # Handle XML name spaces
+    ns = {'xi': 'http://www.w3.org/2001/XInclude'}
+    for k, v in ns.items():
+        ET.register_namespace(k, v)
+
+    # Collect grids (time steps with functions and mesh info)
+    # The time series is also a grid, with GridType = "Collection" and
+    # CollectionType = "Temporal", the time steps are sub-grids of this
+    grids = []
+    out_tree = None
     for fn in inp_file_names:
         if verbose:
             print('Reading %s' % fn)
+        xml = ET.parse(fn)
+        xdmf = xml.getroot()
 
-        state = ''
-        first_grid = None
-        for line in open(fn, 'rt'):
-            if state == '':
-                if 'CollectionType="Temporal"' in line:
-                    state = 'read_timesteps'
-            elif state == 'read_timesteps':
-                if '<Grid' in line:
-                    state = 'read_timestep'
-                    ts_data = line
-                if '</Grid>' in line:
-                    state = ''
-            elif state == 'read_timestep':
-                ts_data += line
-                if '<Time' in line and 'Value=' in line:
-                    t = line.split('Value="')[1].split('"')[0]
-                    t = float(t)
-                if '</Grid>' in line:
-                    timesteps.append((t, first_grid, ts_data))
-                    if first_grid is None:
-                        first_grid = ts_data
-                    state = 'read_timesteps'
+        # Use the first file as a template for the output
+        if out_tree is None:
+            out_tree = ET.parse(fn)
+            out_domain = out_tree.getroot().find('Domain')
+            out_domain.clear()
 
-    timesteps.sort()
+        # Get the time step for this grid and store which previous
+        # grid contains the mesh data (assumed to be the first grid)
+        for tselem in xdmf.findall('.//Grid[@CollectionType="Temporal"]'):
+            grid_with_mesh = None
+            for gridelem in tselem.findall('Grid'):
+                t = float(gridelem.find('Time[@Value]').attrib['Value'])
+                grids.append((t, grid_with_mesh, gridelem))
+                if grid_with_mesh is None:
+                    grid_with_mesh = gridelem
 
-    # Correct xi:include indices
-    timesteps2 = []
-    indices = {}
+    # Sort grids by simulation time step
+    grids.sort()
+
+    # Correct xi:include references
+    grids2 = []
     i = 0
-    for _, first_grid, grid in timesteps:
+    indices = {}
+    for t, grid_with_mesh, grid in grids:
         i += 1
-        if first_grid is None:
+        if grid_with_mesh is None:
             indices[grid] = i
         else:
-            idx = indices[first_grid]
-            grid = grid.replace('Grid[1]', 'Grid[%d]' % idx)
-        timesteps2.append(grid)
+            idx = indices[grid_with_mesh]
+            inc = grid.find('xi:include', ns)
+            ptr = inc.get('xpointer')
+            inc.set('xpointer', ptr.replace('Grid[1]', 'Grid[%d]' % idx))
+        grids2.append(grid)
+
+    # Merge HDF5 files
+    print('\nCopying datasets to merged HDF5 file')
+    current_h5_file = source = None
+    h5_out_file = out_file_name.replace('.xdmf', '.h5')
+    with h5py.File(h5_out_file, 'w') as dest:
+        counters = {}
+        for grid in grids2:
+            for ds in grid.findall('.//DataItem[@Format="HDF"]'):
+                filename, pth = ds.text.rsplit(':', 1)
+                prefix = 'meshdata' if 'Mesh' in pth else 'vec'
+                counters[prefix] = counters.get(prefix, -1) + 1
+                new_pth = '/%s_%05d' % (prefix, counters[prefix])
+
+                if filename != current_h5_file:
+                    if source is not None:
+                        source.close()
+                    print('Reading %s' % filename)
+                    source = h5py.File(filename, 'r')
+                    current_h5_file = filename
+
+                source.copy(pth, dest, new_pth)
+                ds.text = '%s:%s' % (h5_out_file, new_pth)
+        source.close()
+        print('Writing %s' % h5_out_file)
 
     if verbose:
-        print(
-            '\nFound %d time steps (from t=%r to t=%r)'
-            % (len(timesteps), timesteps[0][0], timesteps[-1][0])
-        )
+        print('\nFound %d time steps (from t=%r to t=%r)' % (len(grids), grids[0][0], grids[-1][0]))
         print('Writing %s' % out_file_name)
 
-    with open(out_file_name, 'wt') as out:
-        out.write('<?xml version="1.0"?>\n')
-        out.write('<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>\n')
-        out.write('<Xdmf Version="3.0" xmlns:xi="http://www.w3.org/2001/XInclude">\n')
-        out.write('  <Domain>\n')
-        out.write('    <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">\n')
-        for ts_data in timesteps2:
-            out.write(ts_data)
-        out.write('    </Grid>\n')
-        out.write('  </Domain>\n')
-        out.write('</Xdmf>\n')
+    out_ts_elem = ET.SubElement(out_domain, 'Grid')
+    out_ts_elem.set('Name', 'TimeSeries')
+    out_ts_elem.set('GridType', 'Collection')
+    out_ts_elem.set('CollectionType', 'Temporal')
+    for grid in grids2:
+        out_ts_elem.append(grid)
+    out_tree.write(out_file_name, xml_declaration=True)
 
 
 def error(msg):
